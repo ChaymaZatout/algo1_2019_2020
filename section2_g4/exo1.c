c4c03470c909d8d2e6c7b79d10ac5f7ea08b72bd
chim <zatout.chayma@gmail.com>
2020-01-17 23:00:17 +0100

    Renamed files

--- section2_g4/exo1.c
+++ section2_g4/exo1.c
@@ -1,64 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main() {
+   int fn_2 = 0,fn_1 = 1,fn=0,i,n;
 
-int main()
-{
-    const int TAILLE=200;
-    float t1[TAILLE], t2[TAILLE], s;
-    int n,m,i,j;
+   printf("Donnez une valeur de N\nN=");
+   scanf("%i",&n);
 
-    // Lecture de la taille du premier tableau:
-    do{
-        printf("Donner la taille du 1er tablelau (0 < taille < %d): ", TAILLE);
-        scanf("%d", &n);
-    }while(n<1 || n> TAILLE);
+   if(n==1){
+       // f(1)=1
+       fn = fn_1;
+   }
+   for(i=2;i<=n;i++){
+       fn= (i-1)*fn_1+(n-2)*fn_2; 
+       fn_2 = fn_1; 
+       fn_1 = fn;
+   }
 
-    //Lecture des éléments du premier tableau:
-    printf("Donner les éléments du 1er tableau: \n");
-    for(i=0; i<n; i++){
-        printf("T1[%d] = ", i+1);
-        scanf("%f", &t1[i]);
-    }
-
-    // Lecture de la taille du 2me tableau:
-    do{
-        printf("Donner la taille du 2me tablelau (0 < taille < %d): ", TAILLE+1);
-        scanf("%d", &m);
-    }while(m<1 || m> TAILLE);
-
-    //Lecture des éléments du premier tableau:
-    printf("Donner les éléments du 2me tableau: \n");
-    for(i=0; i<m; i++){
-        printf("T2[%d] = ", i+1);
-        scanf("%f", &t2[i]);
-    }
-
-
-
-
-    //affichage du 1er tableau:
-    printf("\nT1:\n");
-    for(i=0; i<n; i++){
-        printf("%.3f | ", t1[i]);
-
-    }
-
-    //affichage du 2me tableau:
-     printf("\nT2:\n");
-    for(i=0; i<m; i++){
-        printf("%.3f | ", t2[i]);
-
-    }
-
-    //calcule de la somme:
-    s=0;
-    for(j=0; j<m; j++){
-        for(i=0; i<n; i++){
-            s+= t2[j]*t1[i];
-        }
-    }
-
-    printf("\nS= %.3f", s);
-
-    return 0;
+   printf("F(%i) = %i\n",n,fn);
+   
+   return 0;
 }
